d07c3ab3cadec1c1828fbba9b1d43f8a184dbfdb
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-17 14:26:45 +0100

    Styling List Items

--- 02-diving-into-the-basics/rn-complete-guide/App.js
+++ 02-diving-into-the-basics/rn-complete-guide/App.js
@@ -37,8 +37,8 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
-      </View>
+          {courseGoals.map((goal) =>  <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
+        </View>
     </View>
   );
 }
@@ -57,6 +57,13 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
 
