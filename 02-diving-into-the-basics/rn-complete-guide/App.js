e276a5c92e655557417dba20caa20c6109d7f18c
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-17 10:26:41 +0100

    Working with State & Events

--- 02-diving-into-the-basics/rn-complete-guide/App.js
+++ 02-diving-into-the-basics/rn-complete-guide/App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  
+  const addGoalHandler = () => {
+
+    //setCourseGoals([...setCourseGoals, enteredGoal]);
+
+    /**
+     * Same as the above but this is always guaranteed to give you
+     * the latest state snapshot before it then applies your state
+     */
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course goal" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          placeholder="Course goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+
       </View>
     </View>
   );
