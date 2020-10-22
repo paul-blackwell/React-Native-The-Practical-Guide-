908333acafd3af15ecf6754c65ef7abb0d485bc1
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-22 09:59:20 +0100

    Switching Between Multiple "Screens"

--- 04-components-styling-layouts-guss-a-number-app/guessANumberApp/screens/StartGameScreen.js
+++ 04-components-styling-layouts-guss-a-number-app/guessANumberApp/screens/StartGameScreen.js
@@ -49,7 +49,7 @@ const StartGameScreen = props => {
                 <NumberContainer>
                     {selectedNumber}
                 </NumberContainer>
-                <Button title="START GAME" />
+                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
             </Card>
         );
     }
