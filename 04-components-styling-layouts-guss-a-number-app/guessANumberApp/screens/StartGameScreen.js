049d6de97b5c315196ec7345e20d1d6af28f2da8
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-26 08:54:05 +0000

    Custom Wrapper Components & Global Styles

--- 04-components-styling-layouts-guss-a-number-app/guessANumberApp/screens/StartGameScreen.js
+++ 04-components-styling-layouts-guss-a-number-app/guessANumberApp/screens/StartGameScreen.js
@@ -4,7 +4,10 @@ import { View, StyleSheet, Text, TextInput, Button, TouchableWithoutFeedback, Ke
 import Card from '../components/Card';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
+import TitleText from '../components/TitleText';
+import BodyText from '../components/BodyText';
 import Colors from '../constants/colors';
+
 
 const StartGameScreen = props => {
 
@@ -59,9 +62,9 @@ const StartGameScreen = props => {
             Keyboard.dismiss();
         }}>
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game! </Text>
+                <TitleText style={styles.title}>Start a New Game! </TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input
                         style={styles.input}
                         blurOnSubmit
