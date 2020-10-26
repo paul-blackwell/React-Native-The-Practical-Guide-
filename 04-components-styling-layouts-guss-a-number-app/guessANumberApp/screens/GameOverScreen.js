b0f865ec3a37e552cb26305d490aca8967bab225
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-26 11:35:14 +0000

    Styling Images

--- 04-components-styling-layouts-guss-a-number-app/guessANumberApp/screens/GameOverScreen.js
+++ 04-components-styling-layouts-guss-a-number-app/guessANumberApp/screens/GameOverScreen.js
@@ -4,14 +4,21 @@ import { View, StyleSheet, Button, Image } from 'react-native';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
 
+
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <TitleText>The game is over!</TitleText>
-            <Image source={require('../assets/images/success.png')} />
+            <View style={styles.imageContainer}>
+                <Image
+                    source={require('../assets/images/success.png')}
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            </View>
             <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
             <BodyText>Number was: {props.userNumber}</BodyText>
-            <Button title="NEW GAME" onPress={props.onRestart}/>
+            <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     )
 };
@@ -21,7 +28,20 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
 });
 
 export default GameOverScreen;
