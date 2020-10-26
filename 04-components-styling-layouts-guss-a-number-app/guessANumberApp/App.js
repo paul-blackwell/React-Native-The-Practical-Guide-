9c082e4d5ea2a79f51f6c9ebfe0e0a3ca32d7543
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-26 11:23:46 +0000

    Adding Local Images

--- 04-components-styling-layouts-guss-a-number-app/guessANumberApp/App.js
+++ 04-components-styling-layouts-guss-a-number-app/guessANumberApp/App.js
@@ -48,6 +48,9 @@ export default function App() {
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
+  // Just for testing
+  content = (<GameOverScreen roundsNumber={1} userNumber={1} onRestart={configureNewGameHandler} />);
+
   if (userNumber && guessRounds <= 0) {
     content = (<GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />);
   } else if (guessRounds > 0) {
