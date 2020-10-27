2946353ca8b3820311aa9f99b0a6a26f072703fb
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-27 10:50:55 +0000

    ScrollView & Flexbox (Yes, that works!)

--- 04-components-styling-layouts-guss-a-number-app/guessANumberApp/screens/GameScreen.js
+++ 04-components-styling-layouts-guss-a-number-app/guessANumberApp/screens/GameScreen.js
@@ -92,8 +92,8 @@ const GameScreen = props => {
                     <Ionicons name="md-add" size={24} color="white" />
                 </MainButton>
             </Card>
-            <View style={styles.list}>
-                <ScrollView>
+            <View style={styles.listContainer}>
+                <ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
                 </ScrollView>
             </View>
@@ -114,9 +114,14 @@ const styles = StyleSheet.create({
         width: 400,
         maxWidth: '90%',
     },
-    list: {
+    listContainer: {
         flex: 1, // if ScrollView is a child of a view you need this to make it scroll on android
         width: '80%'
+    },
+    list: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
     },
     listItem: {
         borderColor: '#ccc',
@@ -125,7 +130,8 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         backgroundColor: 'white',
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        width: '60%'
     }
 });
 
