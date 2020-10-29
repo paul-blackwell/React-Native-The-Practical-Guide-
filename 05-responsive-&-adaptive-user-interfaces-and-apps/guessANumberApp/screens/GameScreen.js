1069870f8d870066735c90ca6feb9351ae95ad3f
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-29 09:36:42 +0000

    Using Dimensions in "if" Checks

--- 05-responsive-&-adaptive-user-interfaces-and-apps/guessANumberApp/screens/GameScreen.js
+++ 05-responsive-&-adaptive-user-interfaces-and-apps/guessANumberApp/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, ScrollView, FlatList } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView, FlatList, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 
@@ -126,14 +126,15 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%',
     },
     listContainer: {
         flex: 1, // if ScrollView is a child of a view you need this to make it scroll on android
         // width: '80%',
-        width: '60%'
+        width: Dimensions.get('window').width > 350 ? '60%' : '80%',
+
     },
     list: {
         flexGrow: 1,
