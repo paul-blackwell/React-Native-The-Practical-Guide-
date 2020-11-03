c4afe20badfa979492d7b243a613f575082de462
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-03 09:14:12 +0000

    Navigating Between Screens

--- 06-navigation-with-react-navigation/meals-app/screens/CategoriesScreen.js
+++ 06-navigation-with-react-navigation/meals-app/screens/CategoriesScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 
 const CategoriesScreen = props => {
+
     return (
         <View style={styles.screen}>
             <Text>The Categories Screen</Text>
+            <Button title="Go to Meals" onPress={() => {
+                props.navigation.navigate({routeName: 'CategoryMeals'});
+            }}/>
         </View>
     )
 };
