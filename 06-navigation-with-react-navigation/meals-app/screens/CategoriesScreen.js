8f4c1157d67add05b52b258f304144391b917e9f
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-04 22:10:11 +0000

    Configuring the Header with Navigation Options

--- 06-navigation-with-react-navigation/meals-app/screens/CategoriesScreen.js
+++ 06-navigation-with-react-navigation/meals-app/screens/CategoriesScreen.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity, Platform } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
-const renderGridItem = (itemData) => {
-    return (
-        <View style={styles.gridItem}>
-            <Text>{itemData.item.title}</Text>
-        </View>
-    )
-};
 
 
 const CategoriesScreen = props => {
+
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity style={styles.gridItem} onPress={() => {
+                props.navigation.navigate({ routeName: 'CategoryMeals' })
+            }}>
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    };
 
     return (
         <FlatList
@@ -23,12 +29,20 @@ const CategoriesScreen = props => {
     )
 };
 
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+};
+
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    }, 
+    },
     gridItem: {
         flex: 1,
         margin: 15,
