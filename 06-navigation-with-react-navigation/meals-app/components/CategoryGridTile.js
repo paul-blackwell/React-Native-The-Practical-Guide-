e3258771a27f9edbb4f3089d00d04fc7736c0549
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-10 08:16:05 +0000

    Fixing the Shadows

--- 06-navigation-with-react-navigation/meals-app/components/CategoryGridTile.js
+++ 06-navigation-with-react-navigation/meals-app/components/CategoryGridTile.js
@@ -24,7 +24,8 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150, 
         borderRadius: 10,
-        overflow: 'hidden'
+        overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden': 'visible',
+        elevation: 5,
     },
     container: {
         flex: 1,
@@ -32,7 +33,6 @@ const styles = StyleSheet.create({
         shadowColor: 'black',
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 10,
-        elevation: 3,
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
