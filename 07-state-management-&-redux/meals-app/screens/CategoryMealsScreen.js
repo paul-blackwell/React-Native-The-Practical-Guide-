2af011bceae4d77c541b42a77e6e1e27fa0c0c4f
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-17 19:28:10 +0000

    Dispatching Filter Actions

--- 07-state-management-&-redux/meals-app/screens/CategoryMealsScreen.js
+++ 07-state-management-&-redux/meals-app/screens/CategoryMealsScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native'
 import { useSelector } from 'react-redux';
 
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealsList';
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealsScreen = props => {
 
@@ -11,6 +13,14 @@ const CategoryMealsScreen = props => {
     const availableMeals = useSelector(state => state.meals.filteredMeals);
 
     const displayedMeals = availableMeals.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
+    if(displayedMeals.length === 0) {
+        return(
+            <View style={styles.content}>
+                <DefaultText>No meals found, maybe check your filters?</DefaultText>
+            </View>
+        )
+    }
 
     return <MealList listData={displayedMeals} navigation={props.navigation} />
 };
@@ -26,6 +36,14 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     };
 };
 
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 
 export default CategoryMealsScreen;
--- 07-state-management-&-redux/meals-app/screens/FiltersScreen.js
+++ 07-state-management-&-redux/meals-app/screens/FiltersScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useDispatch } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import Colors from '../constants/Colors';
+import { setFilters } from '../store/actions/meals';
 
 
 const FilterSwitch = props => {
@@ -29,15 +31,17 @@ const FiltersScreen = props => {
     const [isVegan, setIsVegan] = useState(false);
     const [isVegetarian, setIsVegetarian] = useState(false);
 
+    const dispatch = useDispatch();
+
     const saveFilters = useCallback(() => {
         const appliedFilters = {
             glutenFree: isGlutenFree,
             isLactoseFree: isLactoseFree,
             vegan: isVegan,
-            isVegetarian: isVegetarian
+            vegetarian: isVegetarian
         };
-        console.log(appliedFilters);
-    }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+        dispatch(setFilters(appliedFilters));
+    }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian, dispatch]);
 
     /**
      * This will allow the appliedFilters to be
