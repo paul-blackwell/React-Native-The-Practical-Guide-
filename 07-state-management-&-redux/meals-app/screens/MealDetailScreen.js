33f48fe4c1321f4975adbd6cc6a02ff37a58ec58
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-17 14:51:38 +0000

    Switching the Favorites Icon (50%)

--- 07-state-management-&-redux/meals-app/screens/MealDetailScreen.js
+++ 07-state-management-&-redux/meals-app/screens/MealDetailScreen.js
@@ -19,6 +19,7 @@ const MealDetailScreen = props => {
 
     const availableMeals = useSelector(state => state.meals.filteredMeals);
     const mealId = props.navigation.getParam('mealId');
+    const currentMealIsFavorite = useSelector(state => state.meals.favoriteMeals.some(meal => meal.id === mealId));
 
     const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
@@ -36,6 +37,10 @@ const MealDetailScreen = props => {
         // props.navigation.setParams({ mealTitle: selectedMeal.title });
         props.navigation.setParams({toggleFav: toggleFavoriteHandler})
     }, [toggleFavoriteHandler]);
+
+    useEffect(() => {
+       props.navigation.setParams({isFav: currentMealIsFavorite}) 
+    },[currentMealIsFavorite]);
 
     return (
         <ScrollView>
@@ -57,7 +62,8 @@ const MealDetailScreen = props => {
 MealDetailScreen.navigationOptions = (navigationData) => {
     // const mealId = navigationData.navigation.getParam('mealId');
     const mealTitle = navigationData.navigation.getParam('mealTitle');
-    const toggleFavorite = navigationData.navigation.getParam('toggleFav')
+    const toggleFavorite = navigationData.navigation.getParam('toggleFav');
+    const isFavorite = navigationData.navigation.getParam('isFav');
     // const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
     return {
@@ -66,7 +72,7 @@ MealDetailScreen.navigationOptions = (navigationData) => {
             <HeaderButtons HeaderButtonComponent={HeaderButton}>
                 <Item
                     title='Favorite'
-                    iconName='ios-star'
+                    iconName={isFavorite ? 'ios-star' : 'ios-star-outline'}
                     onPress={toggleFavorite} />
             </HeaderButtons>
         )
