ab24ff97a313f4d132c91f8239ce7c053945f048
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-18 12:28:17 +0000

    The Products Overview Screen

--- 08-the-shop-app/shop-app/App.js
+++ 08-the-shop-app/shop-app/App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productsReducer
+});
+
+const store = createStore(store);
 
 export default function App() {
   return (
-    <View>
-      <Text>A new app!</Text>
-    </View>
+    <Provider store={store}>
+      <View>...</View>
+    </Provider>
   );
 }
 
