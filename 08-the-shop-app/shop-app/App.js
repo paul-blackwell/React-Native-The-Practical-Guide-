f8100fca59b0d566990e2bac6ce83145d3b1875d
Paul Blackwell <blackwellpaul34@gmail.com>
2020-11-18 11:20:29 +0000

    Creating the Basic Project Setup

---
+++ 08-the-shop-app/shop-app/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>A new app!</Text>
+    </View>
+  );
+}
+
