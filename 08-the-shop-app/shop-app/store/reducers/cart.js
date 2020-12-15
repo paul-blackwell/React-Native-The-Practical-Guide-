e44b4bd97e83a4a654716ed386befc4d7a68e8c0
Paul Blackwell <blackwellpaul34@gmail.com>
2020-12-15 13:32:39 +0000

    Adding Items to the Cart

--- 08-the-shop-app/shop-app/store/reducers/cart.js
+++ 08-the-shop-app/shop-app/store/reducers/cart.js
@@ -1,6 +1,7 @@
 import { ADD_TO_CART } from "../actions/cart";
 
-import {ADD_TO_CART} from '../actions/cart';
+import { ADD_TO_CART } from '../actions/cart';
+import CartItem from '../../models/cart-item';
 
 
 const initialState = {
@@ -16,6 +17,44 @@ export default (state = initialState, action) => {
             const addedProduct = action.product;
             const prodPrice = addedProduct.price;
             const prodTitle = addedProduct.title;
+
+            if (state.items[addedProduct.id]) {
+                // already have the item in the cart
+                const updatedCartItem = newCartItem(
+                    state.items[addedProduct.id].quantity + 1,
+                    prodPrice,
+                    prodTitle,
+                    state.items[addedProduct.id].sum + prodPrice
+                );
+
+                /**
+               * Return a copy of our state,
+               * set items equal to a new object, where we copy all of the existing state items,
+               * then we add a new key, where the key name is addedProduct.id and the value being our
+               * updatedCartItem
+               */
+                return {
+                    ...state,
+                    items: { ...state.items, [addedProduct.id]: updatedCartItem },
+                    totalAmount: state.totalAmount + prodPrice
+                }
+
+            } else {
+                // add a new item 
+                const newCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+
+                /**
+                 * Return a copy of our state,
+                 * set items equal to a new object, where we copy all of the existing state items,
+                 * then we add a new key, where the key name is addedProduct.id and the value being our
+                 * newCartItem
+                 */
+                return {
+                    ...state,
+                    items: { ...state.items, [addedProduct.id]: newCartItem },
+                    totalAmount: state.totalAmount + prodPrice
+                }
+            }
     }
     return state;
 };
